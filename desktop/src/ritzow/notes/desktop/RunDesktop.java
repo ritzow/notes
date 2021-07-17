c37e1ab29f8785ae8ccac8c211bffa661950a3c2
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2021-07-17 12:45:32 -0700

    Make rounded corners for app

--- desktop/src/ritzow/notes/desktop/RunDesktop.java
+++ desktop/src/ritzow/notes/desktop/RunDesktop.java
@@ -6,8 +6,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class RunDesktop {
 	public static void main(String[] args) {
@@ -15,9 +24,29 @@ public class RunDesktop {
 	}
 
 	public static void start() {
-		Stage stage = new Stage();
+		Stage stage = new Stage(StageStyle.TRANSPARENT);
+		stage.setTitle("Online Notes Service");
 		try(var in = Files.newInputStream(Path.of("ui").resolve("main.fxml"))) {
 			stage.setScene(new Scene(new FXMLLoader(StandardCharsets.UTF_8).load(in)));
+			stage.getScene().setFill(Color.TRANSPARENT);
+			/* Transparent edges: */
+			/* https://stackoverflow.com/questions/45258138/round-corners-in-java-fx-pane */
+			/* https://apilevel.wordpress.com/2012/10/11/java-fx-stage-with-rounded-corners-with-background-image/ */
+			Node toolbar = stage.getScene().lookup("#toolbar");
+			Node closeButton = stage.getScene().lookup("#closeButton");
+			Node uploadButton = stage.getScene().lookup("#uploadButton");
+			Node notesContent = stage.getScene().lookup("#notesContent");
+			toolbar.setOnMousePressed(pressEvent -> {
+				toolbar.setOnMouseDragged(dragEvent -> {
+					stage.setX(dragEvent.getScreenX() - pressEvent.getSceneX());
+					stage.setY(dragEvent.getScreenY() - pressEvent.getSceneY());
+				});
+			});
+			if(closeButton instanceof Button button) {
+				button.setOnAction(event -> {
+					stage.close();
+				});
+			}
 			stage.show();
 		} catch(IOException e) {
 			e.printStackTrace();
