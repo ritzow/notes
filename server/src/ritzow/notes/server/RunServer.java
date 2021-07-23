8cc2e0f735ef5dbcf8db7f66d9ff8f7205c0d522
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2021-07-22 22:03:02 -0700

    Server shenanigans

--- server/src/ritzow/notes/server/RunServer.java
+++ server/src/ritzow/notes/server/RunServer.java
@@ -1,89 +1,127 @@
+//for stored procedure: https://sqlperformance.com/2020/09/locking/upsert-anti-pattern
+
 package ritzow.notes.server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.StandardProtocolFamily;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-//import org.hsqldb.server.*;
+import java.sql.*;
+import org.hsqldb.server.Server;
 
 public class RunServer {
-	public static void main(String[] args) throws SQLException, IOException {
-//		Server server = new Server();
-//		server.setDatabaseName(0, "notesdb");
-//		server.setDatabasePath(0, "file:notes.db");
-//		server.setAddress("::1");
-//		server.setPort(5433);
-//		server.start();
+	public static void main(String[] args) throws SQLException {
+		Server server = new Server();
+		server.setErrWriter(null);
+		server.setLogWriter(null);
+		server.setSilent(true);
+		server.setTrace(false);
+		server.setTls(false);
+		server.setDatabaseName(0, "notes");
+		server.setDatabasePath(0, "file:testdb/db");
+		server.setAddress("::1");
+		server.start();
 
-		System.out.println("Program started");
+		//Connection con = DriverManager.getConnection("jdbc:hsqldb:file:server/db;shutdown=true", "SA", "");
 
-		Connection con = DriverManager.getConnection("jdbc:hsqldb:file:db/notes.db;shutdown=true", "SA", "");
-
-		System.out.println("Database started");
+		Connection db = DriverManager.getConnection("jdbc:hsqldb:hsql://[::1]/notes");
 
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			try(var st = con.prepareStatement("SHUTDOWN")) {
-				System.out.println("Shutting down");
-				st.executeUpdate();
-			} catch(SQLException throwables) {
-				throwables.printStackTrace();
-			}
+
 		}));
 
-		var listen4 = ServerSocketChannel
-			.open(StandardProtocolFamily.INET)
-			.bind(new InetSocketAddress("127.0.0.1", 5432))
-			.configureBlocking(false);
-		var listen6 = ServerSocketChannel
-			.open(StandardProtocolFamily.INET6)
-			.bind(new InetSocketAddress("::1", 5432))
-			.configureBlocking(false);
+		System.out.println("Server started.");
 
 		try(Selector select = Selector.open()) {
-			listen4.register(select, SelectionKey.OP_ACCEPT);
-			listen6.register(select, SelectionKey.OP_ACCEPT);
+			ServerSocketChannel
+				.open(StandardProtocolFamily.INET)
+				.bind(new InetSocketAddress("127.0.0.1", 5432))
+				.configureBlocking(false)
+				.register(select, SelectionKey.OP_ACCEPT);
+			ServerSocketChannel
+				.open(StandardProtocolFamily.INET6)
+				.bind(new InetSocketAddress("::1", 5432))
+				.configureBlocking(false)
+				.register(select, SelectionKey.OP_ACCEPT);
+			do {
+				listen(select, db);
+			} while(true);
+		} catch(IOException | SQLException e) {
+			e.printStackTrace();
+		}
 
-			while(true) {
-				select.select();
+		try(var st = db.prepareStatement("SHUTDOWN")) {
+			System.out.println("Shutting down");
+			st.executeUpdate();
+		} catch(SQLException throwables) {
+			throwables.printStackTrace();
+		}
+	}
 
-				for(var key : select.selectedKeys()) {
-					if(key.channel() instanceof ServerSocketChannel serv) {
-						SocketChannel connection = serv.accept();
-						connection.configureBlocking(false);
-						connection.register(select, SelectionKey.OP_READ, new ConnectionState());
-					} else if(key.channel() instanceof SocketChannel connection) {
-						if(key.isReadable()) {
-							ConnectionState state = (ConnectionState)key.attachment();
-							ProcessState newState = process(connection, state);
-							System.out.println(state.state);
-							switch(newState) {
-								case MALFORMED -> {
-									connection.close();
-									key.cancel();
-									System.err.println("Malformed data from " + connection + " during " + state.state);
-								}
-								case DONE -> {
-									connection.close();
-									key.cancel();
-									System.out.println("Done with " + connection);
-								}
-								default -> state.state = newState;
+	private static void listen(Selector select, Connection db) throws IOException, SQLException {
+		select.select();
+
+		for(var key : select.selectedKeys()) {
+			if(key.channel() instanceof ServerSocketChannel serv) {
+				SocketChannel connection = serv.accept();
+				connection.configureBlocking(false);
+				connection.register(select, SelectionKey.OP_READ, new ConnectionState());
+			} else if(key.channel() instanceof SocketChannel connection) {
+				if(key.isReadable()) {
+					ConnectionState state = (ConnectionState)key.attachment();
+					try {
+						ProcessState newState = process(db, connection, state);
+						switch(newState) {
+							case MALFORMED -> {
+								connection.close();
+								key.cancel();
+								System.err.println("Malformed data from " + connection + " during " + state.state);
 							}
+							case DONE -> {
+								System.out.println("Done with " + connection);
+								connection.close();
+								key.cancel();
+							}
+							default -> state.state = newState;
 						}
-					} else {
-						throw new RuntimeException("Unknown channel " + key.channel());
+					} catch(IOException e) {
+						key.cancel();
+						System.err.println(e.getMessage());
 					}
 				}
+			} else {
+				throw new RuntimeException("Unknown channel " + key.channel());
+			}
+		}
 
-				select.selectedKeys().clear();
+		select.selectedKeys().clear();
+	}
+
+	private static void setupProcedures(Connection db) throws SQLException {
+		db.prepareStatement("""
+			create procedure get_or_create_user(in name varchar(255))
+			""");
+	}
+
+	private static void saveNote(Connection db, byte[] username, CharBuffer note) throws SQLException {
+		//https://devblogs.microsoft.com/azure-sql/the-insert-if-not-exists-challenge-a-solution/
+		String name = new String(username, StandardCharsets.UTF_8);
+		System.out.println(name + ": " + note);
+		//PreparedStatement st = db.prepareStatement("SELECT * FROM USERDATA WHERE USERNAME = ?");
+		//st.setNString(1, name);
+		/* Print out the user ID */
+		try(CallableStatement call = db.prepareCall("call get_or_create_user(?)")) {
+			call.setString(1, name);
+			/* TODO this is a blocking call, would cause problems on a real server like this */
+			call.execute();
+			try(ResultSet userEntry = call.getResultSet()) {
+				System.out.println("User \"" + name + "\" has ID " + userEntry.getInt(1));
 			}
 		}
 	}
@@ -92,7 +130,8 @@ public class RunServer {
 		ProcessState state;
 		private final ByteBuffer readBuffer;
 		private byte[] username;
-		private Integer noteLength;
+		private int noteRead;
+		private ByteBuffer noteBuffer;
 		/* TODO need note storage buffer */
 
 		private ConnectionState() {
@@ -114,7 +153,8 @@ public class RunServer {
 		MAX_USERNAME_LENGTH = 1_0000,
 		MAX_NOTE_LENGTH = 4_128;
 
-	private static ProcessState process(SocketChannel conn, ConnectionState state) throws IOException {
+	private static ProcessState process(Connection db, SocketChannel conn, ConnectionState state)
+		throws IOException, SQLException {
 		switch(state.state) {
 			case READ_USERNAME_LENGTH -> {
 				switch(readyBytes(conn, state.readBuffer, Integer.BYTES)) {
@@ -128,7 +168,7 @@ public class RunServer {
 						return ProcessState.READ_USERNAME_TEXT;
 					}
 					case WAIT -> {
-						return ProcessState.READ_USERNAME_LENGTH;
+						return state.state;
 					}
 					case CLOSE -> {
 						return ProcessState.MALFORMED;
@@ -142,7 +182,7 @@ public class RunServer {
 						return ProcessState.READ_NOTE_LENGTH;
 					}
 					case WAIT -> {
-						return ProcessState.READ_USERNAME_TEXT;
+						return state.state;
 					}
 					case CLOSE -> {
 						return ProcessState.MALFORMED;
@@ -156,32 +196,30 @@ public class RunServer {
 						if(noteLength > MAX_NOTE_LENGTH) {
 							return ProcessState.MALFORMED;
 						}
-						state.noteLength = noteLength;
-						state.readBuffer.clear();
+						state.noteBuffer = ByteBuffer.allocate(noteLength);
+						state.noteBuffer.put(state.readBuffer.flip().position(Integer.BYTES));
+						/* read buffer isn't used after this, don't clear it. */
 						return ProcessState.READ_NOTE_TEXT;
 					}
 					case WAIT -> {
-						return ProcessState.READ_NOTE_LENGTH;
+						return state.state;
 					}
 					case CLOSE -> {
-						System.out.println("close");
 						return ProcessState.MALFORMED;
 					}
 				}
 			}
 			case READ_NOTE_TEXT -> {
-				if(state.noteLength == 0) {
-					return ProcessState.DONE;
-				}
-
-				if(conn.read(state.readBuffer) == -1) {
+				if(conn.read(state.noteBuffer) == -1) {
 					return ProcessState.MALFORMED;
 				}
 
-				System.out.println("From " + conn.getRemoteAddress() + ":\"" +
-					StandardCharsets.UTF_8.decode(state.readBuffer.flip()) + "\"");
-				/* TODO read all data, won't always return done in one go, or at all! (might close early) */
-				return ProcessState.DONE;
+				if(!state.noteBuffer.hasRemaining()) {
+					CharBuffer note = StandardCharsets.UTF_8.decode(state.noteBuffer.flip());
+					saveNote(db, state.username, note);
+					return ProcessState.DONE;
+				}
+				return state.state;
 			}
 			case DONE, MALFORMED -> throw new IllegalStateException();
 		}
